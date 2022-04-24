2ac16eed58b3736cbef6d69e585553c45d8c7d6c
Cobgo <mehdiamor@protonmail.com>
2022-04-24 22:45:31 +0200

    obstacles

--- tof.h
+++ tof.h
@@ -1,1 +1,17 @@
-hello
+/*
+ * tof.h
+ *
+ *  Created on: Apr 23, 2022
+ * 		Author: mehdiamor
+ */
+
+#ifndef TOF_H_
+#define TOF_H_
+
+void start_tof(void);
+bool get_freeze(void);
+void set_freeze(bool frozen);
+void wait_detectStart(void);
+
+
+#endif /* TOF_H_ */
