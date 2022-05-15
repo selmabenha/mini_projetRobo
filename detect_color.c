49f314c17020f01e466b75b7a0f0d7291a1ead6e
Cobgo <mehdiamor@protonmail.com>
2022-05-15 20:33:10 +0200

    finalize detect color

--- detect_color.c
+++ detect_color.c
@@ -5,6 +5,7 @@
  *      Author: mehdiamor
  */
 
+
 #include "ch.h"
 #include "hal.h"
 #include <chprintf.h>
@@ -18,31 +19,124 @@
 #include <audio/play_melody.h>
 
 #define IMAGE_BUFFER_SIZE		640
-#define WIDTH_SLOPE_RED				500 //experimental value
-#define MIN_LINE_WIDTH_RED			200 //experimental value
-#define WIDTH_SLOPE_GREEN				500 //experimental value
-#define MIN_LINE_WIDTH_GREEN			200 //experimental value
-#define WIDTH_SLOPE_BLUE				500 //experimental value
-#define MIN_LINE_WIDTH_BLUE			200 //experimental value
-
+#define WIDTH_SLOPE				10
+#define MIN_LINE_WIDTH			50
 #define PXTOCM					1570.0f //experimental value
 #define GOAL_DISTANCE 			10.0f
 #define MAX_DISTANCE 			25.0f
-#define DETECT_NUM				5    //experimental value
-#define BLUE_FILTER				0xF0 //experimental value //0xF0
-#define RED_FILTER				0x1E //experimental value //0x1F
+#define DETECT_NUM				2
+#define BLUE_FILTER				0xF0
+#define RED_FILTER				0x1E
 
 static bool pathFound = 0;
 static bool impasseFound = 0;
 
+static uint8_t red_image[IMAGE_BUFFER_SIZE] = {0};
+static uint8_t blue_image[IMAGE_BUFFER_SIZE] = {0};
+
 //semaphore
 static BSEMAPHORE_DECL(image_ready_sem, TRUE);
 
-/*
- *  Returns the line's width extracted from the image buffer given
- *  Returns 0 if line not found
- */
-bool verify_line_color(uint8_t *buffer, uint16_t width, uint16_t slope) {
+
+static THD_WORKING_AREA(waCaptureImage, 256);
+static THD_FUNCTION(CaptureImage, arg) {
+
+    chRegSetThreadName(_FUNCTION_);
+    (void)arg;
+
+	//Takes pixels 0 to IMAGE_BUFFER_SIZE of the line 10 + 11 (minimum 2 lines because reasons)
+	po8030_advanced_config(FORMAT_RGB565, 0, 10, IMAGE_BUFFER_SIZE, 2, SUBSAMPLING_X1, SUBSAMPLING_X1);
+	dcmi_enable_double_buffering();
+	dcmi_set_capture_mode(CAPTURE_ONE_SHOT);
+	dcmi_prepare();
+
+    while(1){
+        //starts a capture
+		dcmi_capture_start();
+		//waits for the capture to be done
+		wait_image_ready();
+		//signals an image has been captured
+		chBSemSignal(&image_ready_sem);
+    }
+}
+
+
+static THD_WORKING_AREA(waProcessImage, 1024);
+static THD_FUNCTION(ProcessImage, arg) {
+
+    chRegSetThreadName(_FUNCTION_);
+    (void)arg;
+
+    uint8_t *img_buff_ptr;
+
+    bool red_lineCheck[DETECT_NUM] = {0};
+    bool blue_lineCheck[DETECT_NUM] = {0};
+    set_pathFound(0);
+    set_impasseFound(0);
+
+    bool send_to_computer = true;
+
+	while(1){
+
+		//detection process is verified in an array of size DETECT_NUM to help eliminate noise
+    	for(int i = 0; i < DETECT_NUM; i++) {
+    		//waits until an image has been captured
+    		chBSemWait(&image_ready_sem);
+    		//gets the pointer to the array filled with the last image in RGB565
+    		img_buff_ptr = dcmi_get_last_image_ptr();
+
+    		//filters the image into the blue and red tables
+    		filter_image(img_buff_ptr);
+
+    		//search for a line in the image
+    		red_lineCheck[i] = verify_line_color(red_image);
+    		blue_lineCheck[i] = verify_line_color(blue_image);
+    	}
+
+
+    	//finalizes the detection of the red or blue line so that they can be read from the ControlMovement thread
+    	if(send_to_computer){
+    		if(read_table(blue_lineCheck)) {
+    			set_pathFound(true);
+    		}
+    		if(read_table(red_lineCheck)) {
+    			set_impasseFound(true);
+    		}
+    	}
+    	//invert the bool and reinitialize
+    	send_to_computer = !send_to_computer;
+    	for(int i = 0; i < DETECT_NUM; i++) {
+    		red_lineCheck[i] = 0;
+    		blue_lineCheck[i] = 0;
+    	}
+   	}
+}
+
+void detect_color_start(void) {
+	chThdCreateStatic(waProcessImage, sizeof(waProcessImage), NORMALPRIO, ProcessImage, NULL);
+	chThdCreateStatic(waCaptureImage, sizeof(waCaptureImage), NORMALPRIO, CaptureImage, NULL);
+}
+
+void detect_color_end(void) {
+	chThdTerminate(ProcessImage);
+	chThdTerminate(CaptureImage);
+}
+
+bool get_pathFound() {
+	return pathFound;
+}
+void set_pathFound(bool path) {
+	pathFound = path;
+}
+
+bool get_impasseFound() {
+	return impasseFound;
+}
+void set_impasseFound(bool imp) {
+	impasseFound = imp;
+}
+
+bool verify_line_color(uint8_t *buffer) {
 	uint16_t i = 0, begin = 0, end = 0;
 	uint8_t stop = 0;
 	uint32_t mean = 0;
@@ -93,72 +187,6 @@ bool verify_line_color(uint8_t *buffer, uint16_t width, uint16_t slope) {
 	}
 }
 
-static THD_WORKING_AREA(waCaptureImage, 256);
-static THD_FUNCTION(CaptureImage, arg) {
-
-    chRegSetThreadName(__FUNCTION__);
-    (void)arg;
-
-	//Takes pixels 0 to IMAGE_BUFFER_SIZE of the line 10 + 11 (minimum 2 lines because reasons)
-	po8030_advanced_config(FORMAT_RGB565, 0, 10, IMAGE_BUFFER_SIZE, 2, SUBSAMPLING_X1, SUBSAMPLING_X1);
-	dcmi_enable_double_buffering();
-	dcmi_set_capture_mode(CAPTURE_ONE_SHOT);
-	dcmi_prepare();
-
-    while(1){
-        //starts a capture
-		dcmi_capture_start();
-		//waits for the capture to be done
-		wait_image_ready();
-		//signals an image has been captured
-		chBSemSignal(&image_ready_sem);
-    }
-}
-
-
-static THD_WORKING_AREA(waProcessImage, 1024);
-static THD_FUNCTION(ProcessImage, arg) {
-
-    chRegSetThreadName(__FUNCTION__);
-    (void)arg;
-
-	uint8_t *img_buff_ptr;
-	bool red_lineCheck[DETECT_NUM] = {0};
-	bool blue_lineCheck[DETECT_NUM] = {0};
-	set_pathFound(0);
-
-	bool send_to_computer = true;
-
-	while(1){
-
-		//detection process is verified in an array of size DETECT_NUM to help eliminate noise
-		for(int i = 0; i < DETECT_NUM; i++) {
-			//waits until an image has been captured
-			chBSemWait(&image_ready_sem);
-			//gets the pointer to the array filled with the last image in RGB565
-			img_buff_ptr = dcmi_get_last_image_ptr();
-
-			//filters the image into the blue and red tables
-			filter_image(img_buff_ptr);
-
-			//search for a line in the image
-			red_lineCheck[i] = verify_line_color(red_image);
-			blue_lineCheck[i] = verify_line_color(blue_image);
-		}
-
-		//finalizes the detection of the red or blue line so that they can be read from the ControlMovement thread
-		if(send_to_computer){
-			set_led(1,1);
-		}
-		send_to_computer = !send_to_computer;
-		for(int i = 0; i < DETECT_NUM; i++) {
-			red_lineCheck[i] = 0;
-			blue_lineCheck[i] = 0;
-		}
-
-	}
-}
-
 void filter_image(uint8_t *img_ptr) {
 	for(uint16_t i = 0 ; i < (2 * IMAGE_BUFFER_SIZE) ; i+=2){
 		//Extracts only the red pixels - fills the blue table
@@ -178,14 +206,10 @@ bool read_table(bool table[]) {
 	return true;
 }
 
-void process_image_start(void){
-	chThdCreateStatic(waProcessImage, sizeof(waProcessImage), NORMALPRIO+1, ProcessImage, NULL);
-	chThdCreateStatic(waCaptureImage, sizeof(waCaptureImage), NORMALPRIO+1, CaptureImage, NULL);
-}
 
-bool get_pathFound(void) {
-	return pathFound;
-}
-void set_pathFound(bool path) {
-	pathFound = path;
-}
+
+
+
+
+
+
