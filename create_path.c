d6cba729dcc861ca5b76731448ee9759911a2a36
Cobgo <mehdiamor@protonmail.com>
2022-05-15 19:53:12 +0200

    finalize create path

--- create_path.c
+++ create_path.c
@@ -34,32 +34,38 @@ static bool end_of_path = 0;
 static bool turn_around = 0;
 static uint8_t game_over = 0;
 
+
 static THD_WORKING_AREA(theControlMovement, 256);
 static THD_FUNCTION(ControlMovement, arg) {
 
-	chRegSetThreadName(__FUNCTION__);
-	(void)arg;
+    chRegSetThreadName(_FUNCTION_);
+    (void)arg;
 
-	systime_t time;
+    systime_t time;
 
-	while(!get_end_of_path()){
-		time = chVTGetSystemTime();
-		//once a red or blue line has been detected, the corresponding bool value must stay true until the action has been completed at
-		//the wall, where the line may not be detectable anymore
-		if(!get_end_of_path()) {set_end_of_path(get_pathFound());}
-		if(!get_turn_around()) {set_turn_around(get_impasseFound());}
+    while(!get_end_of_path()){
+        time = chVTGetSystemTime();
+    	//once a red or blue line has been detected, the corresponding bool value must stay true until the action has been completed at
+    	//the wall, where the line may not be detectable anymore
+    	if(!get_end_of_path()) {set_end_of_path(get_pathFound());}
+    	if(!get_turn_around()) {set_turn_around(get_impasseFound());}
 
 
-		init_path(get_max_norm_index());
-		//100Hz
-		chThdSleepUntilWindowed(time, time + MS2ST(10));
+    	init_path(get_max_norm_index());
+    	//100Hz
+    	chThdSleepUntilWindowed(time, time + MS2ST(10));
 
-		//defines the end of the program once the countdown runs to the end
-		set_end_of_path(time_path());
+    	//defines the end of the program once the countdown runs to the end
+    	set_end_of_path(time_path());
 
-	}
+    }
+
+    while(get_end_of_path()) {
+    	motors_stop();
+    }
 }
 
+//begins control movement thread
 void control_mov_start(void) {
 	chThdCreateStatic(theControlMovement, sizeof(theControlMovement), NORMALPRIO, ControlMovement, NULL);
 }
@@ -76,6 +82,9 @@ void init_path(int16_t index) {
 		process_path(index);
 	} else {
 		motors_go();
+		set_pathFound(false);
+		set_impasseFound(false);
+		set_turn_around(false);
 	}
 
 }
@@ -89,23 +98,27 @@ void process_path(int16_t index) {
 	//turn right
 	else if(index >= FREQ_RIGHT_L && index <= FREQ_RIGHT_H){
 		motors_turn_right();
-	}
-	//is a line found?
-	else if(get_pathFound()) {
+	//spin and stop
+	} else if(get_end_of_path()){
+		motors_stop();
 		motors_spin();
+		motors_stop();
 		playMelody(MARIO_FLAG, 0, NULL);
 		waitMelodyHasFinished();
-		set_led(1,1);
-	} else if(get_impasseFound()) {
-		motors_u_turn();
+	//turn around
+	} else if(get_turn_around()){
+		motors_stop();
 		playMelody(WALKING, 0, NULL);
-		waitMelodyHasFinished();
-		set_led(3,1);
+		motors_u_turn();
+		set_impasseFound(false);
+		turn_around = false;
 	}
 }
 
-void time_path(void) {
-	if(!get_pathFound()) {
+//creates a countdown timer for the epuck
+bool time_path(void) {
+	if(!get_end_of_path()) {
+
 		if(!chVTIsSystemTimeWithinX(0, GAME_OVER_TIME) && game_over == 0) {
 			set_led(1,1);
 			playNote(NOTE_C5, WARN_TIME);
@@ -133,3 +146,17 @@ void time_path(void) {
 	clear_leds();
 	return false;
 }
+
+bool get_end_of_path(void) {
+	return end_of_path;
+}
+void set_end_of_path(bool end) {
+	end_of_path = end;
+}
+bool get_turn_around(void) {
+	return turn_around;
+}
+void set_turn_around(bool turn) {
+	turn_around = turn;
+}
+
