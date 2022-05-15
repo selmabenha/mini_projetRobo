77275701ba86481503bcf5cb44ec6aec76faebc1
selmabenha <selmabenha@gmail.com>
2022-05-15 12:48:05 +0200

    finalize main

--- main.c
+++ main.c
@@ -1,25 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
 
 #include "ch.h"
 #include "hal.h"
 #include "memory_protection.h"
 #include <usbcfg.h>
 #include <main.h>
+#include <chprintf.h>
+#include <motors_pro.h>
 #include <motors.h>
+
+#include <audio/microphone.h>
+#include <audio_processing.h>
+#include <fft.h>
+#include <communications.h>
+#include <arm_math.h>
+
+#include <audio/play_melody.h>
+
+#include <sensors/VL53L0X/VL53L0X.h>
+#include <tof.h>
+
 #include <camera/po8030.h>
 #include <chprintf.h>
 
-#include <pi_regulator.h>
-#include <process_image.h>
+#include <create_path.h>
+#include <detect_color.h>
 
-void SendUint8ToComputer(uint8_t* data, uint16_t size) 
+//allows printing of image graph
+void SendUint8ToComputer(uint8_t* data, uint16_t size)
 {
-	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)"START", 5);
-	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)&size, sizeof(uint16_t));
-	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)data, size);
+	chSequentialStreamWrite((BaseSequentialStream )&SD3, (uint8_t)"START", 5);
+	chSequentialStreamWrite((BaseSequentialStream )&SD3, (uint8_t)&size, sizeof(uint16_t));
+	chSequentialStreamWrite((BaseSequentialStream )&SD3, (uint8_t)data, size);
 }
 
 static void serial_start(void)
@@ -37,40 +51,41 @@ static void serial_start(void)
 int main(void)
 {
 
-	halInit();
-	chSysInit();
-	mpu_init();
+    halInit();
+    chSysInit();
+    mpu_init();
 
-	//initializes melody
+    //initializes melody
 	playMelodyStart();
 
-	//starts the serial communication
-	serial_start();
-	//starts the USB communication
-	usb_start();
-	//inits the motors
-	motors_init();
-	//starts the TOF sensors
-	VL53L0X_start();
-	//start the TOF thread
-	start_tof();
+    //starts the serial communication
+    serial_start();
+    //starts the USB communication
+    usb_start();
+    //inits the motors
+    motors_init();
+    //starts the TOF sensors
+    VL53L0X_start();
+    //start the TOF thread
+    start_tof();
 
-	//starts the camera
-	dcmi_start();
-	po8030_start();
-	po8030_set_awb(0);
-	//starts the threads for the processing of the image
-	detect_color_start();
+    //starts the camera
+    dcmi_start();
+   	po8030_start();
+   	po8030_set_awb(0);
+   	//starts the threads for the processing of the image
+   	detect_color_start();
 	dac_start();
 
-	//starts the threads for the control of the motors and audio processing
-	control_mov_start();
-	control_audio_start();
+    //starts the threads for the control of the motors and audio processing
+    control_mov_start();
+    control_audio_start();
 
-	/* Infinite loop. */
-	while (1) {
-		wait_send_to_computer();
-	}
+    /* Infinite loop. */
+    while (1) {
+    	wait_send_to_computer();
+    }
+
 }
 
 #define STACK_CHK_GUARD 0xe2dee396
