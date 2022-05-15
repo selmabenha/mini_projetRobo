c5e06cb84366229c7ebf29661e573b0c4503531a
selmabenha <selmabenha@gmail.com>
2022-05-15 21:53:11 +0200

    finalize header files

--- detect_color.h
+++ detect_color.h
@@ -8,14 +8,25 @@
 #ifndef DETECT_COLOR_H_
 #define DETECT_COLOR_H_
 
-void process_image_start(void);
-bool verify_line_color(uint8_t *buffer, uint16_t width, uint16_t slope);
+//start and end of image capture & processing threads
+void detect_color_start(void);
+void detect_color_end(void);
 
+//setter and getter of static bool variables
 bool get_pathFound(void);
 void set_pathFound(bool path);
+bool get_impasseFound(void);
+void set_impasseFound(bool imp);
 
-void filter_image(uint8_t *img_ptr);
-
+//checks that all values of an array are true
 bool read_table(bool table[]);
 
-#endif /* DETECT_COLOR_H */
+//applies a red and blue mask to the image and places the  result in static arrays
+void filter_image(uint8_t *img_ptr);
+
+//function that detects whether a colored bar is present on a white background
+//white includes red, green, and blue pixels
+//a colored bar would indicate a lack of red, green, or blue pixels
+bool verify_line_color(uint8_t *buffer);
+
+#endif /* DETECT_COLOR_H_ */
